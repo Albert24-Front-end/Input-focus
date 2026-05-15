9ab448d6bae9442b3c579b461848a48a5100c08d
Albert <albertaliev777@gmail.com>
2026-05-15 10:47:03 +0500

    simple input focus

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useIdUpload } from './composables/useIdUpload';
+
+const { isLoading, isIdProvided, isExtraSpellingNeeded, upload } = useIdUpload();
+
+const vFocus = {
+  mounted(el: HTMLInputElement) {
+    el.focus()
+  }
+}
+
+</script>
+
+<template>
+  <h1>Identification Form</h1>
+  <button :disabled="isLoading" @click="upload">
+    <template v-if="!isLoading">Provide ID</template>
+    <template v-else>Loading</template>
+  </button>
+  <p v-if="isIdProvided">
+    <label>Provide your name in English</label><span v-if="isExtraSpellingNeeded" class="required">(required)</span>:
+    <input v-focus/>
+  </p>
+</template>
+
+<style>
+.required{
+  color: red;
+}
+</style>
